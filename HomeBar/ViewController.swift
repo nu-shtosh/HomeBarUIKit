f0cf5a79cefe14b60c82870aa1203e70433973cb
ILIA <dubenskiiilia@gmail.com>
2022-10-28 17:38:54 +0200

    Initial Commit

---
+++ HomeBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeBar
+//
+//  Created by Илья Дубенский on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
