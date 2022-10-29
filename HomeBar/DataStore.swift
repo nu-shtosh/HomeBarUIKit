318ea3cb6e8dced66c75336b4950b5f0c02c2402
Alexandr-Mayyura <sonickill@yandex.ru>
2022-10-29 23:22:03 +0300

    добавил своего теста

--- HomeBar/DataStore.swift
+++ HomeBar/DataStore.swift
@@ -44,3 +44,20 @@ struct DataStore {
 
     private init() { }
 }
+
+struct DrinkDataStore {
+    static let shared = DrinkDataStore()
+
+    let drinks = [
+        "Водка": 0.33,
+        "Пиво": 0.045,
+        "Сухое вино": 0.13,
+        "Крепленое вино": 0.17,
+        "Шампанское": 0.13,
+        "Джин": 0.41,
+        "Виски": 0.42,
+        "Ром": 0.334
+    ]
+
+    private init() { }
+}
