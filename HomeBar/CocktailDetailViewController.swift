8f0f998971c667f6183dafcc9cb744c628940a71
Andrey Abakumov <andrey9864rus@mail.ru>
2022-10-30 20:33:07 +0400

    work with CocktailDetailViewController

--- HomeBar/CocktailDetailViewController.swift
+++ HomeBar/CocktailDetailViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class CocktailDetailViewController: UIViewController {
 
+    @IBOutlet var cocktailNameLabel: UILabel!
+    @IBOutlet var alcoholLabel: UILabel!
+    
+    @IBOutlet var cocktailImage: UIImageView!
+    
+    @IBOutlet var ingredientsTextView: UITextView!
+    @IBOutlet var recipeTextView: UITextView!
+    @IBOutlet var factTextView: UITextView!
+    
+    var cocktail: Cocktail!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        cocktailNameLabel.text = cocktail.name
+        alcoholLabel.text = String(cocktail.alcohol)
+        recipeTextView.text = cocktail.recipe
+        factTextView.text = cocktail.info
+        ingredientsTextView.text = cocktail.ingredients.joined(separator: ", ")
+        cocktailImage.image = UIImage(named: cocktail.image)
     }
     
-
+    @IBAction func closeButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
     /*
     // MARK: - Navigation
 
--- HomeBar/PossibleCocktailsTableViewController.swift
+++ HomeBar/PossibleCocktailsTableViewController.swift
@@ -8,20 +8,24 @@
 import UIKit
 
 class PossibleCocktailsTableViewController: UITableViewController {
-
+    
     var possibleCocktails: [Cocktail] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
         navigationBarSettings()
     }
-
+    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         possibleCocktails.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(
             withIdentifier: "possibleCocktail",
@@ -48,6 +52,12 @@ class PossibleCocktailsTableViewController: UITableViewController {
         content.text = cocktail.name
         cell.contentConfiguration = content
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let cocktailDetailVC = segue.destination as? CocktailDetailViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        cocktailDetailVC.cocktail = possibleCocktails[indexPath.row]
     }
 }
 
